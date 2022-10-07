d6e7cf4191f07be6e12028e043e27a73c4dd0dc9
cesio97 <114251017+cesio97@users.noreply.github.com>
2022-10-07 16:47:47 +0300

    Add files via upload

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,7 @@
+﻿// на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Введите N");
+int N = Convert.ToInt32 (Console.ReadLine ());
+
+for (int i = 1; i <= N; i++)
+     if (i % 2 == 0)
+    Console.WriteLine (i);
