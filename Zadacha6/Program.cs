0cca7f4337ed413157fe731cf85cec7a1708cf8b
cesio97 <114251017+cesio97@users.noreply.github.com>
2022-10-07 16:46:52 +0300

    Add files via upload

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,12 @@
+﻿// На вход принимает число и выдает, является ли число четным
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32 (Console.ReadLine ());
+if ((a % 2) == 0)
+
+{
+    Console.WriteLine ("Да");
+}    
+else
+{
+   Console.WriteLine ("Нет"); 
+}
