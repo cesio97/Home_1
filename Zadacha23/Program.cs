5ecd973f5ecc2b8c4337219cda326c6e78831bf0
cesio97 <114251017+cesio97@users.noreply.github.com>
2022-10-14 00:09:09 +0300

    Add files via upload

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cube(int n)
+{   
+    int i = 1;
+    while (i <= n)
+    {
+        Console.WriteLine($"куб числа {i} = {Math.Pow(i,3)}");
+        i++;
+    }
+}
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Cube(number);
